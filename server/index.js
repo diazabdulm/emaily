40651c97f14e434ef9c3c87187118b8dd58005fa
Abdul <abdul.diaz@fcb.com>
2020-04-17 13:37:38 -0400

    fixed typo in server

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
 
   const path = require("path");
   app.get("*", (require, response) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
